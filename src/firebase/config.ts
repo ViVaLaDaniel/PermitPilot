6e6040db864114e58072a67b4746c3a78f47ee05
Daniel Zamiatin <zamyatin.daniel@gmail.com>
2026-01-03 15:36:56 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1687532129-4c643",
+  "appId": "1:873999023004:web:02045e86ee9d4511ebe659",
+  "apiKey": "AIzaSyAVCzCC0fEpOq7rGpXFDFC29ZN6-C8lL-o",
+  "authDomain": "studio-1687532129-4c643.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "873999023004"
+};
